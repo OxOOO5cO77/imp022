c7ad2625352556f564ec9259daeeed2bf7c01af0
Scott Barcik <oxooo5co77@impending.org>
2025-01-11 13:01:42 -0800

    Prevent link traversal for closed links  = Fix logic error in link processing and discard set_current result
    Signed-off-by: Scott Barcik <oxooo5co77@impending.org>

--- crates/hall/src/private/logic/server/choose_intent/intents/link.rs
+++ crates/hall/src/private/logic/server/choose_intent/intents/link.rs
@@ -1,11 +1,12 @@
-use hall::core::MissionNodeLinkDir;
+use hall::core::{MissionNodeLinkDir, MissionNodeLinkState};
 
 use crate::private::game::{GameMission, GameUser};
 
 pub(crate) fn process_intent(mission: &mut GameMission, user: &mut GameUser, dir: MissionNodeLinkDir) -> bool {
-    mission //
+    mission // note: we don't care if the target node is new, just that we traversed successfully
         .get_node(user.mission_state.current())
         .and_then(|mission| mission.links.iter().find(|n| n.direction == dir))
+        .filter(|link| link.state == MissionNodeLinkState::Open)
         .map(|link| user.mission_state.set_current(link.target))
-        .unwrap_or(false)
+        .is_some()
 }
